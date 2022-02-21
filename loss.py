0dc891862981b2932f7731f553589e526a386aad
roshni714 <roshni714@github.com>
2022-02-21 10:28:56 -0800

    basic network and loss

---
+++ loss.py
@@ -0,0 +1,21 @@
+def squared_loss(y_hat, y_true):
+    return (y_hat - y_true) ** 2
+
+LOSS_DIC={"squared_loss": squared_loss}
+
+class RockafellarUryasevLoss:
+
+    def __init__(self, loss, bound_function):
+        self.loss = LOSS_DIC[loss]
+        self.bound_function = bound_function
+
+    def __call__(self, y, h_out, alpha_out):
+
+        bound_low = bound_function(x, low=True)
+        bound_up = bound_function(x, up=True)
+
+        loss_vals = self.loss(h_out, y)
+
+        ru_loss = bound_low * loss_vals + (1- bound_low) * alpha_out + (bound_up - bound_low) * torch.nn.functional.relu(loss_vals - alpha_out)
+        return torch.mean(ru_loss)
+
---
+++ modules/basic.py
@@ -0,0 +1,74 @@
+mport torch
+import torchvision.models as models
+from pytorch_lightning.core.lightning import LightningModule
+from metrics import Metrics
+from losses import GaussianNLL
+from distributions import GaussianDistribution
+from modules.cnn import CNN
+
+
+class BasicModel(LightningModule):
+    def __init__(self, input_size, y_scale):
+        super().__init__()
+        self.alpha_net = torch.nn.Sequential(
+                torch.nn.Linear(input_size, 100),
+                torch.nn.ReLU(),
+                torch.nn.Linear(100, 100),
+                torch.nn.ReLU(),
+                torch.nn.Linear(100, 1),
+            )
+        self.h_net = torch.nn.Sequential(
+            torch.nn.Linear(input_size, 100),
+            torch.nn.ReLU(),
+            torch.nn.Linear(100, 100),
+            torch.nn.ReLU(),
+            torch.nn.Linear(100, 1))
+        self.y_scale = y_scale
+
+    def forward(self, x):
+        h_out = self.h_net(x)
+        alpha_out = self.alpha_net(x)
+        return h_out, alpha_out
+
+    def training_step(self, batch, batch_idx):
+        x, y = batch
+        h_out, alpha_out = self(x)
+        l = RockafellarUryasevLoss(y, h_out, alpha_out)
+        tensorboard_logs = {"train_loss": l}
+        return {"loss": l, "log": tensorboard_logs}
+
+    def configure_optimizers(self):
+        optimizer = torch.optim.Adam(self.parameters(), lr=1e-3)
+        return optimizer
+
+    def validation_step(self, batch, batch_idx):
+        x, y = batch
+        h_out, alpha_out = self(x)
+        l = RockafellarUryasevLoss(y, h_out, alpha_out)
+        return {"val_loss": l}
+
+    def validation_epoch_end(self, outputs):
+        avg_loss = torch.stack([x["val_loss"] for x in outputs]).mean()
+        tensorboard_logs = {}
+        for key in outputs[0]:
+            cal = torch.stack([x[key] for x in outputs]).mean()
+            tensorboard_logs[key] = cal
+        return {"val_loss": avg_loss, "log": tensorboard_logs}
+
+    def test_step(self, batch, batch_idx):
+        x, y = batch
+        h_out, alpha_out = self(x)
+        dic = {
+            "test_loss": RockafellarUryasevLoss(y, h_out, alpha_out),
+        }
+        return dic
+
+    def test_epoch_end(self, outputs):
+        avg_loss = torch.stack([x["test_loss"] for x in outputs]).mean()
+        tensorboard_logs = {}
+        for key in outputs[0]:
+            cal = torch.stack([x[key] for x in outputs]).mean()
+            tensorboard_logs[key] = cal
+            setattr(self, key, float(cal))
+        return {"test_loss": avg_loss, "log": tensorboard_logs}
+
